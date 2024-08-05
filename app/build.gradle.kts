0bcfd5adf9c99f7d0e35f74e078f20d47f7105fa
dantr3 <danielfdezcilla04@gmail.com>
2024-08-04 19:27:01 -0600

    Activity design & DB details

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation(files("libs\\jtds-1.3.1.jar"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/proyecto/sistemaventaspropat/MainActivity.kt
+++ app/src/main/java/com/proyecto/sistemaventaspropat/MainActivity.kt
@@ -1,43 +1,28 @@
 package com.proyecto.sistemaventaspropat
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.proyecto.sistemaventaspropat.databinding.ActivityMainBinding
+import com.proyecto.sistemaventaspropat.databinding.ActivitySignupBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var etNombre: EditText
-    private lateinit var etApellido: EditText
-    private lateinit var etCorreo: EditText
-    private lateinit var etContrasena: EditText
-    private lateinit var btnIniciarSesion: Button
+    private lateinit var binding: ActivityMainBinding
+    private val SIGNUP_REQUEST_CODE = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        // Initialize views
-        etNombre = findViewById(R.id.etNombre)
-        etApellido = findViewById(R.id.etApellido)
-        etCorreo = findViewById(R.id.etCorreo)
-        etContrasena = findViewById(R.id.etContrasena)
-        btnIniciarSesion = findViewById(R.id.btnIniciarSesion)
 
-        // Set click listener for the button
-        btnIniciarSesion.setOnClickListener {
-            val nombre = etNombre.text.toString().trim()
-            val apellido = etApellido.text.toString().trim()
-            val correo = etCorreo.text.toString().trim()
-            val contrasena = etContrasena.text.toString().trim()
-
-            if (nombre.isEmpty() || apellido.isEmpty() || correo.isEmpty() || contrasena.isEmpty()) {
-                Toast.makeText(this, "Por favor, complete todos los campos.", Toast.LENGTH_SHORT).show()
-            } else {
-                // Handle login logic here
-                Toast.makeText(this, "Iniciando sesión para $nombre $apellido", Toast.LENGTH_SHORT).show()
-            }
-        }
     }
+
+
+
 }
