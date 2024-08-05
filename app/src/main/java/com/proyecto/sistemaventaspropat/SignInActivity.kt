5bdb99b8db7c5aa6f7a0c33f727e6f890a9bae62
dantr3 <danielfdezcilla04@gmail.com>
2024-08-04 20:53:52 -0600

    New updates...

--- app/src/main/java/com/proyecto/sistemaventaspropat/SignInActivity.kt
+++ app/src/main/java/com/proyecto/sistemaventaspropat/SignInActivity.kt
@@ -2,10 +2,11 @@ package com.proyecto.sistemaventaspropat
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.proyecto.sistemaventaspropat.databinding.ActivitySigninBinding
 
-class SignInActivity : AppCompatActivity(){
+class SignInActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySigninBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,7 +15,18 @@ class SignInActivity : AppCompatActivity(){
         setContentView(binding.root)
 
         binding.btnSignin.setOnClickListener {
-            startActivity(Intent(this, HomePageActivity::class.java))
+            val id = binding.txtId.text.toString()
+            val password = binding.txtPassword.text.toString()
+
+            if (id.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Por favor complete todos los campos", Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+                Toast.makeText(this, "Signup successful!", Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this, HomePageActivity::class.java))
+            }
+
+
         }
         binding.txtSignup.setOnClickListener {
             startActivity(Intent(this, SignUpActivity::class.java))
